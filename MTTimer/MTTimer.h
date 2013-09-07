ba3739814063f82081b7e91a4885ce25d74dc71b
Adam Kirk <atomkirk@gmail.com>
2013-09-07 07:22:04 -0600

    Initial Commit

---
+++ MTTimer/MTTimer.h
@@ -0,0 +1,13 @@
+//
+//  MTTimer.h
+//  MTTimer
+//
+//  Created by Adam Kirk on 9/7/13.
+//  Copyright (c) 2013 Mysterious Trousers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTTimer : NSObject
+
+@end
---
+++ MTTimer/MTTimer.m
@@ -0,0 +1,13 @@
+//
+//  MTTimer.m
+//  MTTimer
+//
+//  Created by Adam Kirk on 9/7/13.
+//  Copyright (c) 2013 Mysterious Trousers. All rights reserved.
+//
+
+#import "MTTimer.h"
+
+@implementation MTTimer
+
+@end
