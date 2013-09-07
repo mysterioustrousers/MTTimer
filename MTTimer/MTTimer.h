aec60090863ae12709356fce3f400924cbe049ba
Adam Kirk <atomkirk@gmail.com>
2013-09-07 07:26:29 -0600

    init

--- MTTimer/MTTimer.h
+++ MTTimer/MTTimer.h
@@ -1,13 +1,19 @@
 //
 //  MTTimer.h
-//  MTTimer
+//  Firehose
 //
-//  Created by Adam Kirk on 9/7/13.
+//  Created by Adam Kirk on 6/6/13.
 //  Copyright (c) 2013 Mysterious Trousers. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface MTTimer : NSObject
+
++ (MTTimer *)timerThatMustLastAtLeast:(NSTimeInterval)interval untilPerformingBlock:(void (^)())block;
++ (MTTimer *)timerThatMustLastAtMost:(NSTimeInterval)interval beforePerformingBlock:(void (^)())block;
++ (MTTimer *)timerThatMustLastAtLeast:(NSTimeInterval)min atMost:(NSTimeInterval)max beforePerformingBlock:(void (^)())block;
+
+- (void)done;
 
 @end
--- MTTimer/MTTimer.m
+++ MTTimer/MTTimer.m
@@ -1,13 +1,123 @@
 //
 //  MTTimer.m
-//  MTTimer
+//  Firehose
 //
-//  Created by Adam Kirk on 9/7/13.
+//  Created by Adam Kirk on 6/6/13.
 //  Copyright (c) 2013 Mysterious Trousers. All rights reserved.
 //
 
 #import "MTTimer.h"
 
+
+@interface MTTimer ()
+@property (nonatomic, strong) void       (^block)();
+@property (nonatomic, strong) NSTimer    *minTimer;
+@property (nonatomic, strong) NSTimer    *maxTimer;
+@property (nonatomic, assign) BOOL       isDone;
+@end
+
 @implementation MTTimer
+
+
++ (MTTimer *)timerThatMustLastAtLeast:(NSTimeInterval)interval untilPerformingBlock:(void (^)())block
+{
+    MTTimer *timer  = [MTTimer new];
+    timer.minTimer  = [NSTimer scheduledTimerWithTimeInterval:interval
+                                                       target:timer
+                                                     selector:@selector(nsTimerExpired:)
+                                                     userInfo:nil
+                                                      repeats:NO];
+    timer.maxTimer  = nil;
+    timer.block     = block;
+    timer.isDone    = NO;
+    return timer;
+}
+
++ (MTTimer *)timerThatMustLastAtMost:(NSTimeInterval)interval beforePerformingBlock:(void (^)())block
+{
+    MTTimer *timer  = [MTTimer new];
+    timer.maxTimer  = [NSTimer scheduledTimerWithTimeInterval:interval
+                                                       target:timer
+                                                     selector:@selector(nsTimerExpired:)
+                                                     userInfo:nil
+                                                      repeats:NO];
+    timer.minTimer  = nil;
+    timer.block     = block;
+    timer.isDone    = NO;
+    return timer;
+}
+
++ (MTTimer *)timerThatMustLastAtLeast:(NSTimeInterval)min atMost:(NSTimeInterval)max beforePerformingBlock:(void (^)())block
+{
+    MTTimer *timer  = [MTTimer new];
+    timer.minTimer  = [NSTimer scheduledTimerWithTimeInterval:min
+                                                       target:timer
+                                                     selector:@selector(nsTimerExpired:)
+                                                     userInfo:nil
+                                                      repeats:NO];
+
+    timer.maxTimer  = [NSTimer scheduledTimerWithTimeInterval:max
+                                                       target:timer
+                                                     selector:@selector(nsTimerExpired:)
+                                                     userInfo:nil
+                                                      repeats:NO];
+    timer.block     = block;
+    timer.isDone    = NO;
+    return timer;
+}
+
+
+- (void)done
+{
+    if (_minTimer && !_maxTimer) {
+        if (_minTimer.isValid) {
+            _isDone = YES;
+        }
+        else {
+            if (_block) _block();
+        }
+    }
+
+    else if (_maxTimer && !_minTimer) {
+        if (_maxTimer.isValid) {
+            [_maxTimer fire];
+        }
+    }
+
+    else if (_minTimer && _maxTimer) {
+        if (_minTimer.isValid) {
+            _isDone = YES;
+        }
+        else if (!_minTimer.isValid && _maxTimer.isValid) {
+            [_maxTimer fire];
+        }
+    }
+}
+
+
+
+
+
+#pragma mark - Private
+
+- (void)nsTimerExpired:(NSTimer *)nsTimer
+{
+    if (_minTimer && !_maxTimer) {
+        if (_isDone) {
+            if (_block) _block();
+        }
+    }
+
+    else if (_maxTimer && !_minTimer) {
+        if (_block) _block();
+    }
+
+    else if (_maxTimer && _minTimer) {
+        if (_isDone || nsTimer == _maxTimer) {
+            if (_block) _block();
+        }
+    }
+    
+}
 
 @end
